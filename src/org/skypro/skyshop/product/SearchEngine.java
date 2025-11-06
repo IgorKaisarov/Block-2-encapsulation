146a1645365b5c17de18ef5e5c84692f18270809
IgorKaisarov <Igo444@mail.ru>
2025-11-06 15:23:52 +0600

    Home Work Block 2 encapsulation

--- src/org/skypro/skyshop/product/SearchEngine.java
+++ src/org/skypro/skyshop/product/SearchEngine.java
@@ -2,31 +2,33 @@ package org.skypro.skyshop.product;
 
 public class SearchEngine {
 
-    String Searchable[];
+    Searchable Searchable[];
 
     public Searchable[] search(String searchTerm) {
-        return results;
-    }
-
-    Searchable[] results = new Searchable[5];
-    int count = 0; // Счетчик найденных элементов
-
-    searchables[0] = new Article("В мире животных", "Оглавление");
+        Searchable[] results = new Searchable[5]; int count = 0; // Счетчик найденных элементов
+        searchables[0] = new Article("В мире животных", "Оглавление");
 
 
-    for (Searchable item : searchables) {
-        if (item != null && item.getSearchTerm().contains(searchTerm)) {
-            if(item.getSearchTerm().contains(searchTerm) { } &&count< 5) {
-                results[count] = item;
-                count++;
+        for (Searchable item : searchables) {
+            if (item != null && item.getSearchTerm().contains(searchTerm)) {
+                if(item.getSearchTerm().contains(searchTerm) {
+                } &&count< 5) {
+                    results[count] = item;
+                    count++;
+                }
             }
         }
+        return results; }
+
+
+
+//    public SearchEngine(String[] searchable, CharSequence searchTerm) {
+//        Searchable = searchable;
+//        this.searchTerm = searchTerm;
+//    }
+
+    public SearchEngine(int size) {
+        this.searchables = new Searchable[size];
     }
 
-
-
-    public SearchEngine(String[] searchable, CharSequence searchTerm) {
-        Searchable = searchable;
-        this.searchTerm = searchTerm;
-    }
 }
